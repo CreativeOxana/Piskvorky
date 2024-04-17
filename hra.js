e76a6d16b3cc864487542e77c0877c5108750e7a
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2024-04-17 21:49:17 +0200

    update hra.js

--- hra.js
+++ hra.js
@@ -1,7 +1,6 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
-const fields = document.querySelectorAll('.piskvorky__board--field');
 
 const handleClick = (event) => {
   event.target.disabled = true;
@@ -16,37 +15,37 @@ const handleClick = (event) => {
     document.querySelector('.player').src = 'images/circle.svg';
     document.querySelector('.player').alt = 'kolečko';
   }
+
+  const playBoardElm = Array.from(fields);
+  const playBoard = playBoardElm.map((field) => {
+    if (field.classList.contains('piskvorky__square--circle')) {
+      return 'o';
+    } else if (field.classList.contains('piskvorky__square--cross')) {
+      return 'x';
+    } else {
+      return '_';
+    }
+  });
+
+  const findingWinner = () => {
+    const winner = findWinner(playBoard);
+    if (winner === 'o') {
+      alert(`Vyhrálo kolečko!`);
+      location.reload();
+    } else if (winner === 'x') {
+      alert('Vyhrál křížek!');
+      location.reload();
+    } else if (winner === 'tie') {
+      alert('Hra skončila nerozhodně!');
+      location.reload();
+    }
+  };
+  setTimeout(findingWinner, 500);
 };
 
+const fields = document.querySelectorAll('.piskvorky__board--field');
 fields.forEach((field) => {
   field.addEventListener('click', handleClick);
-});
-
-fields.forEach((field) => {
-  field.addEventListener('click', () => {
-    const playBoard = Array.from(fields).map((field) => {
-      if (field.classList.contains('piskvorky__square--circle')) {
-        return 'o';
-      } else if (field.classList.contains('piskvorky__square--cross')) {
-        return 'x';
-      } else {
-        return '_';
-      }
-    });
-
-    const winnerIs = findWinner(playBoard);
-    const writeWinner = () => {
-      if (winnerIs === 'x' || winnerIs === 'o') {
-        alert(`Vyhrál hráč se symbolem ${winnerIs}.toUpperCase!`);
-        location.reload();
-      } else if (winnerIs === 'tie') {
-        alert('Hra skončila nerozhodně!');
-        location.reload();
-      }
-    };
-
-    setTimeout(writeWinner, 500);
-  });
 });
 
 const restartButton = document.querySelector('.button__restart');
