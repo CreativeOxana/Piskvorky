b93974e03ab6121af07a06a6d6faa6e6641156ac
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2024-04-27 22:13:19 +0200

    Adding AI move

--- hra.js
+++ hra.js
@@ -1,21 +1,9 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
+let playBoard = [];
 
-const handleClick = (event) => {
-  event.target.disabled = true;
-  if (currentPlayer === 'circle') {
-    event.target.classList.add('piskvorky__board--field--circle');
-    currentPlayer = 'cross';
-    document.querySelector('.player').src = 'images/cross.svg';
-    document.querySelector('.player').alt = 'křížek';
-  } else {
-    event.target.classList.add('piskvorky__board--field--cross');
-    currentPlayer = 'circle';
-    document.querySelector('.player').src = 'images/circle.svg';
-    document.querySelector('.player').alt = 'kolečko';
-  }
-
+const makingMove = () => {
   const playBoardElm = Array.from(fields);
   const playBoard = playBoardElm.map((field) => {
     if (field.classList.contains('piskvorky__board--field--circle')) {
@@ -26,22 +14,58 @@ const handleClick = (event) => {
       return '_';
     }
   });
-
-  const winner = findWinner(playBoard);
-  const alertWinner = () => {
-    alert(`Vyhrál hráč se symbolem ${winner}`);
-    location.reload();
-  };
-  const alertTie = () => {
-    alert('Hra skončila nerozhodně!');
-    location.reload();
-  };
-  if (winner === 'x' || winner === 'o') {
-    setTimeout(alertWinner, 300);
-  } else if (winner === 'tie') {
-    setTimeout(alertTie, 300);
-  }
 };
+
+const handleClick = async (event) => {
+  event.target.disabled = true;
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('piskvorky__board--field--circle');
+    makingMove();
+    currentPlayer = 'cross';
+    document.querySelector('.player').src = 'images/cross.svg';
+    document.querySelector('.player').alt = 'křížek';
+  } else {
+    event.target.classList.add('piskvorky__board--field--cross');
+    currentPlayer = 'circle';
+    document.querySelector('.player').src = 'images/circle.svg';
+    document.querySelector('.player').alt = 'kolečko';
+  }
+
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: playBoard,
+        player: 'x',
+      }),
+    },
+  );
+
+  const data = await response.json();
+  const { x, y } = data.position;
+  const fieldIndex = x + y * 10;
+  const field = fields[fieldIndex];
+  field.click();
+};
+
+const winner = findWinner(playBoard);
+const alertWinner = () => {
+  alert(`Vyhrál hráč se symbolem ${winner}`);
+  location.reload();
+};
+const alertTie = () => {
+  alert('Hra skončila nerozhodně!');
+  location.reload();
+};
+if (winner === 'x' || winner === 'o') {
+  setTimeout(alertWinner, 300);
+} else if (winner === 'tie') {
+  setTimeout(alertTie, 300);
+}
 
 const fields = document.querySelectorAll('.piskvorky__board--field');
 fields.forEach((field) => {
